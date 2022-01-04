b8de2bde4293b123bdaa3bed7ad792439ec5db27
AyamotoKohei <koh14taka2@gmail.com>
2022-01-04 23:10:12 +0900

    add: HeaderとLinkの実装

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import Link from "./Link";
+
+const Header = () => {
+  return (
+    <div>
+      <Link href="/">Home</Link>
+      <Link href="/about">About</Link>
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Link.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from "next/router";
+
+const Link = ({ children, href }) => {
+  const router = useRouter();
+
+  return (
+    <a
+      href="#"
+      onClick={(e) => {
+        e.preventDefault();
+        router.push(href);
+      }}
+    >
+      {children}
+      <style jsx>{`
+        a {
+          margin-right: 10px;
+        }
+      `}</style>
+    </a>
+  );
+};
+
+export default Link;
