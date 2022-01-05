e6ede671521045bd2aa323eb71ba700e5d279096
AyamotoKohei <koh14taka2@gmail.com>
2022-01-05 23:30:09 +0900

    add: Aboutページの実装

---
+++ src/pages/about.tsx
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const About = () => {
+  return (
+    <div>
+      <Header />
+      <p>This is the about page.</p>
+    </div>
+  );
+};
+
+export default About;
